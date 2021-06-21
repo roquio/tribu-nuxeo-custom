ee69a4ae89873119fc8eb9b02d52c5c7aef5461d
Loïc Billon <loic.billon@gmail.com>
2021-06-21 17:12:43 +0200

    Fix phantom docs on elasticsearch

--- src/main/java/fr/gouv/education/foad/wf/cleaner/TtcWfCleaner.java
+++ src/main/java/fr/gouv/education/foad/wf/cleaner/TtcWfCleaner.java
@@ -4,32 +4,37 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
+import org.nuxeo.elasticsearch.api.ElasticSearchIndexing;
 import org.nuxeo.elasticsearch.api.ElasticSearchService;
 import org.nuxeo.elasticsearch.query.NxQueryBuilder;
 import org.nuxeo.runtime.api.Framework;
+
+import fr.toutatice.ecm.platform.core.helper.ToutaticeDocumentHelper;
 
 
 public class TtcWfCleaner {
 
 
-    private static final Log log = LogFactory.getLog("nuxeo.wf.cleaner");
-    
+    @Context
+    protected ElasticSearchIndexing esi2;
 
-    
-    public TtcWfCleaner() {
-    	
-    	
-    }
-    
+
+	private static final Log log = LogFactory.getLog("nuxeo.wf.cleaner");
+
     
 	public int doCleanupDoneAndCanceledRouteInstances(String reprositoryName) {
 		UnrestrictedTtcWfCleaner unrestrictedSessionRunner = new UnrestrictedTtcWfCleaner(reprositoryName);
@@ -49,8 +54,11 @@ public class TtcWfCleaner {
         
         private static final String OLD_PROC_INSTANCES = "SELECT * FROM Document WHERE ecm:primaryType = 'ProcedureInstance' AND dc:modified <= DATE '%s' ORDER BY dc:created";        
 
-
+        // compteurs
         protected int i = 0, t = 0, p = 0;
+        
+        // compteurs pour erreurs de dénormalisation
+        protected int iErr = 0, tErr = 0, pErr = 0;
 
     	private int routesLimit;
     	private int proceduresLimit;
@@ -67,7 +75,7 @@ public class TtcWfCleaner {
         public void run() {
         	
 			ElasticSearchService service = Framework.getService(ElasticSearchService.class);
-
+			
 			if (service != null) {
 				NxQueryBuilder queryBuilder = new NxQueryBuilder(session);
 				queryBuilder.nxql(DOCUMENT_ROUTE_DONE_CANCEL);
@@ -95,23 +103,31 @@ public class TtcWfCleaner {
 						}
 						catch(ClientException e) {
 							log.error("Failed to remove task "+task.getId());
+							tErr++;
+							
+							unrefElasticsearchDoc(task.getId());
+							
 						}
 					}
 					if(t > 0) {
-						log.info("Remove "+t+" task(s)");
+						log.info("Remove "+t+" task(s). Unlink "+tErr+ " task(s) on ES ");
 					}
 					
 					try {
 						session.removeDocument(new IdRef(routeDocId));
 						i++;
+						
 					}
 					catch(ClientException e) {
 						log.error("Failed to remove documentroute "+routeDocId);
+						iErr++;
+						
+						unrefElasticsearchDoc(routeDocId);
 					}
 					
 
 	            }
-	            log.info("Remove "+i+" route(s)");
+	            log.info("Remove "+i+" route(s). Unlink "+iErr+ " route(s) on ES ");
 	            
 	    		Date referenceDate = new Date();
 	    		Calendar c = Calendar.getInstance(); 
@@ -139,9 +155,12 @@ public class TtcWfCleaner {
 					}
 					catch(ClientException e) {
 						log.error("Failed to remove procedure "+id);
+						pErr++;
+						
+						unrefElasticsearchDoc(id);
 					}
 				}
-				log.info("Remove "+p+" procedure(s)");
+				log.info("Remove "+p+" procedure(s). Unlink "+pErr+ " procedure(s) on ES ");
 	    		
 			}
 			else {
@@ -153,6 +172,28 @@ public class TtcWfCleaner {
         public int getNumberOfCleanedUpWf() {
             return i;
         }
-    }
+        
+        private void unrefElasticsearchDoc(String rootID) {
+        	          
+            AutomationService automation = Framework.getService(AutomationService.class);
+            
+            OperationContext ctx = new OperationContext(this.session);
+            
+            Map<String, Object> parameters = new HashMap<String, Object>();
+            parameters.put("type", "ROOT");
+            parameters.put("repositoryName", "default");
+            parameters.put("rootID", rootID);
+            
+            
+			try {
+				ToutaticeDocumentHelper.callOperation(automation, ctx, "Document.ReIndexES", parameters );
+			} catch (Exception e) {
+				log.error("Failed to document on ES "+rootID);
 
+			}
+                       
+            
+        }
+    }
+    
 }
