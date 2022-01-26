8c77441e084cde44ae6db196856c5b85406fca47
Loïc Billon <loic.billon@gmail.com>
2022-01-26 16:44:04 +0100

    Workflows ouverts non supprimés.

--- src/main/java/fr/gouv/education/foad/wf/cleaner/TtcWfCleaner.java
+++ src/main/java/fr/gouv/education/foad/wf/cleaner/TtcWfCleaner.java
@@ -1,8 +1,10 @@
 package fr.gouv.education.foad.wf.cleaner;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +20,7 @@ import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
+import org.nuxeo.ecm.core.api.model.impl.primitives.StringProperty;
 import org.nuxeo.elasticsearch.api.ElasticSearchIndexing;
 import org.nuxeo.elasticsearch.api.ElasticSearchService;
 import org.nuxeo.elasticsearch.query.NxQueryBuilder;
@@ -47,14 +50,18 @@ public class TtcWfCleaner {
     	
     	public static final String DOCUMENT_ROUTE_DONE_CANCEL =  "SELECT * FROM DocumentRoute WHERE (ecm:currentLifeCycleState = 'done' "
                 + " OR ecm:currentLifeCycleState = 'canceled') ORDER BY dc:created";
-
-
+    	
         private static final String TASK_RELATED = "SELECT * FROM Document WHERE ecm:mixinType = 'Task' AND nt:processId = '%s'"
               + " AND ecm:isVersion = 0";
         
         private static final String OLD_PROC_INSTANCES = "SELECT * FROM Document WHERE ecm:primaryType = 'ProcedureInstance' AND dc:modified <= DATE '%s' ORDER BY dc:created";        
 
-        private static final String PROC_INVIT_INSTANCES = "SELECT * FROM Document WHERE ecm:primaryType = 'ProcedureInstance' AND dc:created <= DATE '2021-10-02' AND pi:currentStep = 'invitation' ORDER BY dc:created";
+        
+        
+    	public static final String OLD_DOCUMENT_ROUTE_RUNNING =  "SELECT * FROM DocumentRoute WHERE ecm:currentLifeCycleState = 'running' "
+                + " AND docri:variablesFacet = 'facet-var_generic-model' AND dc:created <= DATE '%s' ORDER BY dc:created";  
+        private static final String PROC_RELATED = "SELECT * FROM Document WHERE ecm:primaryType = 'ProcedureInstance' AND ecm:uuid = '%s'";        
+    	
         
         // compteurs
         protected int i = 0, t = 0, p = 0;
@@ -79,57 +86,10 @@ public class TtcWfCleaner {
 			ElasticSearchService service = Framework.getService(ElasticSearchService.class);
 			
 			if (service != null) {
-				NxQueryBuilder queryBuilder = new NxQueryBuilder(session);
-				queryBuilder.nxql(DOCUMENT_ROUTE_DONE_CANCEL);
-				queryBuilder.limit(routesLimit);
-				DocumentModelList results = service.query(queryBuilder);
-
-				List<String> routeIds = new ArrayList<>();
-				for (DocumentModel result : results) {
-					routeIds.add(result.getId());
-				}
+				removeClosedRoutes(service);
 				
+				removeOpenRoutes(service);
 				
-	            for (String routeDocId : routeIds) {
-
-	            	queryBuilder.nxql(String.format(TASK_RELATED, routeDocId));
-	            	
-					DocumentModelList tasks = service.query(queryBuilder);
-					
-					t = 0;
-					for (DocumentModel task : tasks) {
-
-						try {
-							session.removeDocument(new IdRef(task.getId()));
-							t++;
-						}
-						catch(ClientException e) {
-							log.error("Failed to remove task "+task.getId());
-							tErr++;
-							
-							unrefElasticsearchDoc(task.getId());
-							
-						}
-					}
-					if(t > 0) {
-						log.info("Remove "+t+" task(s). Unlink "+tErr+ " task(s) on ES ");
-					}
-					
-					try {
-						session.removeDocument(new IdRef(routeDocId));
-						i++;
-						
-					}
-					catch(ClientException e) {
-						log.error("Failed to remove documentroute "+routeDocId);
-						iErr++;
-						
-						unrefElasticsearchDoc(routeDocId);
-					}
-					
-
-	            }
-	            log.info("Remove "+i+" route(s). Unlink "+iErr+ " route(s) on ES ");
 	            
 	    		Date referenceDate = new Date();
 	    		Calendar c = Calendar.getInstance(); 
@@ -139,10 +99,12 @@ public class TtcWfCleaner {
 	    		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
 	    		String formattedDate = sdf.format(referenceDate);
 	    		
-	    		queryBuilder.nxql(String.format(OLD_PROC_INSTANCES, formattedDate));
-	    		queryBuilder.limit(proceduresLimit);
+	    		NxQueryBuilder queryBuilder2 = new NxQueryBuilder(session);
+	    		
+	    		queryBuilder2.nxql(String.format(OLD_PROC_INSTANCES, formattedDate));
+	    		queryBuilder2.limit(proceduresLimit);
             	
-				DocumentModelList oldPIs = service.query(queryBuilder);
+				DocumentModelList oldPIs = service.query(queryBuilder2);
 				List<String> procids = new ArrayList<>();
 				for (DocumentModel pi : oldPIs) {
 					procids.add(pi.getId());
@@ -163,32 +125,7 @@ public class TtcWfCleaner {
 					}
 				}
 				log.info("Remove "+p+" procedure(s). Unlink "+pErr+ " procedure(s) on ES ");
-				
-				// ==== Procédures avant la refonte hash numenv
-				queryBuilder.nxql(String.format(PROC_INVIT_INSTANCES));
-	    		queryBuilder.limit(proceduresLimit);
-            	
-				DocumentModelList oldInvits = service.query(queryBuilder);
-				procids = new ArrayList<>();
-				for (DocumentModel pi : oldInvits) {
-					procids.add(pi.getId());
-				}
-				
-				
-				for (String id : procids) {
-
-					try {
-						session.removeDocument(new IdRef(id));
-						p++;
-					}
-					catch(ClientException e) {
-						log.error("Failed to remove procedure invitation V1 "+id);
-						pErr++;
-						
-						unrefElasticsearchDoc(id);
-					}
-				}
-				log.info("Remove "+p+" procedure(s) invitation V1. Unlink "+pErr+ " procedure(s) on ES ");				
+	
 	    		
 			}
 			else {
@@ -197,9 +134,147 @@ public class TtcWfCleaner {
             
         }
 
+		private void removeOpenRoutes(ElasticSearchService service) {
+			
+    		Date referenceDate = new Date();
+    		Calendar c = Calendar.getInstance(); 
+    		c.setTime(referenceDate); 
+    		c.add(Calendar.MONTH, -2);
+    		referenceDate = c.getTime();
+    		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+    		String formattedDate = sdf.format(referenceDate);
+			
+			NxQueryBuilder queryBuilder = new NxQueryBuilder(session);
+			queryBuilder.nxql(String.format(OLD_DOCUMENT_ROUTE_RUNNING, formattedDate));
+			queryBuilder.limit(routesLimit);
+			DocumentModelList results = service.query(queryBuilder);
+
+			List<String> routeIds = new ArrayList<>();
+			for (DocumentModel result : results) {
+				
+				Collection<StringProperty> participatingDocuments = (Collection) result.getProperty("docri:participatingDocuments");
+				for(StringProperty participatingDocument : participatingDocuments) {
+					
+					log.info("Check participant "+participatingDocument.getValue());
+					
+					queryBuilder.nxql(String.format(PROC_RELATED, participatingDocument.getValue()));
+					DocumentModelList pis = service.query(queryBuilder);
+					
+					if(pis.size() == 0) {
+						routeIds.add(result.getId());
+					}
+					else {
+						log.info("Skip route "+result.getId()+". A procedure is linked : "+pis.get(0).getId());
+
+					}
+				}			
+			}
+			
+			
+			for (String routeDocId : routeIds) {
+
+				queryBuilder.nxql(String.format(TASK_RELATED, routeDocId));
+				
+				DocumentModelList tasks = service.query(queryBuilder);
+				
+				t = 0;
+				for (DocumentModel task : tasks) {
+
+					try {
+						session.removeDocument(new IdRef(task.getId()));
+						t++;
+					}
+					catch(ClientException e) {
+						log.error("Failed to remove task "+task.getId());
+						tErr++;
+						
+						unrefElasticsearchDoc(task.getId());
+						
+					}
+				}
+				if(t > 0) {
+					log.info("Remove "+t+" opened task(s). Unlink "+tErr+ " task(s) on ES ");
+				}
+				
+				try {
+					session.removeDocument(new IdRef(routeDocId));
+					i++;
+					
+				}
+				catch(ClientException e) {
+					log.error("Failed to remove documentroute "+routeDocId);
+					iErr++;
+					
+					unrefElasticsearchDoc(routeDocId);
+				}
+				
+
+			}
+			log.info("Remove "+i+" opened route(s). Unlink "+iErr+ " route(s) on ES ");
+			
+		}
+
+		private void removeClosedRoutes(ElasticSearchService service) {
+			NxQueryBuilder queryBuilder = new NxQueryBuilder(session);
+			queryBuilder.nxql(DOCUMENT_ROUTE_DONE_CANCEL);
+			queryBuilder.limit(routesLimit);
+			DocumentModelList results = service.query(queryBuilder);
+
+			List<String> routeIds = new ArrayList<>();
+			for (DocumentModel result : results) {
+				routeIds.add(result.getId());
+			}
+			
+			
+			for (String routeDocId : routeIds) {
+
+				queryBuilder.nxql(String.format(TASK_RELATED, routeDocId));
+				
+				DocumentModelList tasks = service.query(queryBuilder);
+				
+				t = 0;
+				for (DocumentModel task : tasks) {
+
+					try {
+						session.removeDocument(new IdRef(task.getId()));
+						t++;
+					}
+					catch(ClientException e) {
+						log.error("Failed to remove task "+task.getId());
+						tErr++;
+						
+						unrefElasticsearchDoc(task.getId());
+						
+					}
+				}
+				if(t > 0) {
+					log.info("Remove "+t+" closed task(s). Unlink "+tErr+ " task(s) on ES ");
+				}
+				
+				try {
+					session.removeDocument(new IdRef(routeDocId));
+					i++;
+					
+				}
+				catch(ClientException e) {
+					log.error("Failed to remove documentroute "+routeDocId);
+					iErr++;
+					
+					unrefElasticsearchDoc(routeDocId);
+				}
+				
+
+			}
+			log.info("Remove "+i+" closed or cancelled route(s). Unlink "+iErr+ " route(s) on ES ");
+		}
+
         public int getNumberOfCleanedUpWf() {
             return i;
         }
+        
+        
+        
+        
         
         private void unrefElasticsearchDoc(String rootID) {
         	          
